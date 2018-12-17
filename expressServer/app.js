044c3aec89d5e138ac65d949f2acb82c1ed49161
zxd <yrqc53719@163.com>
2018-12-18 06:23:40 +0800

    服务端完成

--- expressServer/app.js
+++ expressServer/app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
 
-
+const hero = require('./router/zxd')
 
 
 // 链接上数据库  并给数据库命名myZXD
-var db = mongoose.connect('mongodb://localhost:27017/myDB');
+var db = mongoose.connect('mongodb://localhost:27017/myDB',{
+	useNewUrlParser: true 
+});
 
 
 
@@ -16,6 +18,9 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/api',hero);
+
 app.listen(3001,()=>{
   console.log('app listening on port 3000.')
 })
